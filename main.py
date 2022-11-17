4192acc1038f7d7454f49157108be4c9b431efab
Ptitlu42 <118479475+Ptitlu42@users.noreply.github.com>
2022-11-17 23:28:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,47 @@ TOKEN = 'MTA0Mjc1NDA0ODc3Mzg2OTU3OQ.GOakq5.AUf6-ld_s0zoTF9wsmQurFt56ZdhkIMDJeoLf
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.guilds = True
+
 
 client = commands.Bot(command_prefix='', intents=intents)
 
 
 @client.event
-async def on_ready():
+async def on_ready(): #affiche ready sur le terminal quand le bot est OP
     print("Ready!")
     
 
 @client.event
-async def on_disconnect():
+async def on_disconnect(): #affiche not ready sur le terminal quand le bot est HS
     print("Not ready!")
 
 @client.event
 async def on_message(message):
-        if message.content.lower() == "date":
+        if message.content.lower() == "!date": #afficher date quand date est taper
           await message.channel.send(now.strftime('%H:%M:%S on %A, %B the %dth, %Y'))
-        if message.content.lower() == "ping":
+        if message.content.lower() == "!ping": #afficher pong quand ping est taper
           await message.channel.send("Pong!")
         
+
+@client.event
+async def on_member_join(member):
+    acceuil_channel: discord.TextChannel = client.get_channel(1042922943354773657) #definir sur quel salon agit le member_join et definir le type de salon
+    await acceuil_channel.send(content=f"Bienvenue {member.display_name} !") #envoyer bienvenue à l'arrivé d'un membre dans le discord
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.run ("MTA0Mjc1NDA0ODc3Mzg2OTU3OQ.GOakq5.AUf6-ld_s0zoTF9wsmQurFt56ZdhkIMDJeoLfU")
