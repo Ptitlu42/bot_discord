c94e382a12c0bff2991876d3b7a93db71ad49d98
Ptitlu42 <118479475+Ptitlu42@users.noreply.github.com>
2022-11-18 15:55:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,53 @@
 import discord
+import os
 import datetime
+from dotenv import load_dotenv
 
-from discord.ext import commands
+load_dotenv(dotenv_path="config")
 
 now = datetime.datetime.now()
-
-TOKEN = 'MTA0Mjc1NDA0ODc3Mzg2OTU3OQ.GOakq5.AUf6-ld_s0zoTF9wsmQurFt56ZdhkIMDJeoLfU'
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.guilds = True
 
+client = discord.Client(intents=intents)
 
-client = commands.Bot(command_prefix='', intents=intents)
-
+#affiche ready sur le terminal quand le bot est OP
 
 @client.event
-async def on_ready(): #affiche ready sur le terminal quand le bot est OP
+async def on_ready(): 
     print("Ready!")
     
 
+#affiche not ready sur le terminal quand le bot est HS    
+
 @client.event
-async def on_disconnect(): #affiche not ready sur le terminal quand le bot est HS
+async def on_disconnect(): 
     print("Not ready!")
+
+#afficher date quand !date est taper
 
 @client.event
 async def on_message(message):
-        if message.content.lower() == "!date": #afficher date quand date est taper
+
+        if message.content.lower() == "!date": 
           await message.channel.send(now.strftime('%H:%M:%S on %A, %B the %dth, %Y'))
-        if message.content.lower() == "!ping": #afficher pong quand ping est taper
+
+#afficher pong quand !ping est taper
+
+        if message.content.lower() == "!ping": 
           await message.channel.send("Pong!")
         
+     
+ #envoyer bienvenue à l'arrivé d'un membre dans le discord
 
 @client.event
 async def on_member_join(member):
-    acceuil_channel: discord.TextChannel = client.get_channel(1042922943354773657) #definir sur quel salon agit le member_join et definir le type de salon
-    await acceuil_channel.send(content=f"Bienvenue {member.display_name} !") #envoyer bienvenue à l'arrivé d'un membre dans le discord
-
-    
+    acceuil_channel: discord.TextChannel = client.get_channel(1042922943354773657)     #definir sur quel salon agit le member_join et definir le type de salon
+    await acceuil_channel.send(content=f"Bienvenue {member.display_name} !")          
 
 
 
-
-
-
-
-
-
-
-
-
-
-client.run ("MTA0Mjc1NDA0ODc3Mzg2OTU3OQ.GOakq5.AUf6-ld_s0zoTF9wsmQurFt56ZdhkIMDJeoLfU")
+client.run (os.getenv("discord_token"))
